d6606b79cadc0595d4ef4c20a3d3d0d3ea31d3ab
George Psarakis <giwrgos.psarakis@gmail.com>
2024-11-30 14:32:01 +0200

    [ui] Fix header/navigation bar layout

--- ui/src/Layout.tsx
+++ ui/src/Layout.tsx
@@ -1,24 +1,42 @@
-import {Center, Container, Heading, HStack, Link, LinkBox, LinkOverlay, Separator} from "@chakra-ui/react"
+import {
+    Image,
+    Container,
+    Heading,
+    HStack,
+    Link,
+    LinkBox,
+    LinkOverlay,
+    Flex,
+    Float,
+    Box,
+    Separator
+} from "@chakra-ui/react"
 
 function Layout() {
-    return <Container maxW={"2xl"} marginTop={2} marginBottom={5}>
-        <Center>
-            <HStack>
-                <LinkBox padding={4}>
-                    <LinkOverlay asChild>
-                        <Link href={"/"}>
-                            <Heading color={"teal.500"} size={"xl"}>Random</Heading>
-                        </Link>
-                    </LinkOverlay>
-                </LinkBox>
-                <LinkBox padding={4}>
-                    <LinkOverlay asChild>
-                        <Link href={"/on-this-day/events"}>
-                            <Heading color={"teal.500"} size={"xl"}>On This Day</Heading></Link>
-                    </LinkOverlay>
-                </LinkBox>
-            </HStack>
-        </Center>
+    return <Container fluid marginTop={2} marginBottom={5}>
+        <Flex justifyContent="space-between" gap="4">
+            <LinkBox padding={1}>
+                <LinkOverlay asChild>
+                    <Image height={10} src="/logo192.png" />
+                </LinkOverlay>
+            </LinkBox>
+            <Flex flexGrow={1} justify={"flex-end"}>
+            <LinkBox padding={4}>
+                <LinkOverlay asChild>
+                    <Link href={"/"}>
+                        <Heading color={"teal.500"} size={"md"}>Random</Heading>
+                    </Link>
+                </LinkOverlay>
+            </LinkBox>
+            <LinkBox padding={4}>
+                <LinkOverlay asChild>
+                    <Link href={"/on-this-day/events"}>
+                        <Heading color={"teal.500"} size={"md"}>On This Day</Heading></Link>
+                </LinkOverlay>
+            </LinkBox>
+            </Flex>
+        </Flex>
+        <Separator/>
     </Container>;
 }
 
