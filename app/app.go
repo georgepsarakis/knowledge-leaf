5e0a22e4a241565ba52c14b871e012d788306b6b
George Psarakis <giwrgos.psarakis@gmail.com>
2025-03-03 21:44:37 +0200

    [backend] Introduce Postgres app configuration

--- app/app.go
+++ app/app.go
@@ -2,25 +2,36 @@ package app
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 	"github.com/sethvargo/go-envconfig"
 	"go.uber.org/zap"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type Configuration struct {
-	Port           int           `env:"PORT,default=4000"`
-	AllowedOrigins []string      `env:"ALLOWED_ORIGINS,default=http://localhost"`
-	RedisDSN       string        `env:"REDIS_DSN,default=redis://localhost:6379"`
-	UseRedis       bool          `env:"USE_REDIS,default=false"`
-	RequestTimeout time.Duration `env:"REQUEST_TIMEOUT,default=30s"`
+	Port             int           `env:"PORT,default=4000"`
+	AllowedOrigins   []string      `env:"ALLOWED_ORIGINS,default=http://localhost"`
+	RedisDSN         string        `env:"REDIS_DSN,default=redis://localhost:6379"`
+	UseRedis         bool          `env:"USE_REDIS,default=false"`
+	RequestTimeout   time.Duration `env:"REQUEST_TIMEOUT,default=30s"`
+	PostgresHost     string        `env:"POSTGRES_HOST,default=localhost"`
+	PostgresPort     int           `env:"POSTGRES_PORT,default=5432"`
+	PostgresUser     string        `env:"POSTGRES_USER,default=knowledge_leaf"`
+	PostgresPassword string        `env:"POSTGRES_PASSWORD"`
+	PostgresDatabase string        `env:"POSTGRES_DATABASE,default=knowledge_leaf"`
+	PostgresEnabled  bool          `env:"POSTGRES_ENABLED,default=false"`
 }
 
 type App struct {
-	Cfg         Configuration
-	RedisClient *redis.Client
-	Logger      *zap.Logger
+	Cfg                Configuration
+	RedisClient        *redis.Client
+	Logger             *zap.Logger
+	PostgresConnection *gorm.DB
 }
 
 func New() (App, func() error, error) {
@@ -29,20 +40,37 @@ func New() (App, func() error, error) {
 	envconfig.MustProcess(context.Background(), &cfg)
 	app.Cfg = cfg
 
-	logger, _ := zap.NewProduction()
-	app.Logger = logger
+	appLogger, _ := zap.NewProduction()
+	app.Logger = appLogger
 
 	if cfg.UseRedis {
-		logger.Info("using Redis database")
+		app.Logger.Info("using Redis database")
 		rc, err := newRedisClient(cfg.RedisDSN)
 		if err != nil {
 			return app, nil, err
 		}
 		app.RedisClient = rc
 	}
+	if app.Cfg.PostgresEnabled {
+		dsn := fmt.Sprintf("user=%s password=%s host=%s port=%d dbname=%s",
+			app.Cfg.PostgresUser,
+			app.Cfg.PostgresPassword,
+			app.Cfg.PostgresHost,
+			app.Cfg.PostgresPort,
+			app.Cfg.PostgresDatabase,
+		)
+		db, err := gorm.Open(
+			postgres.New(postgres.Config{DSN: dsn}), &gorm.Config{
+				Logger: logger.Default.LogMode(logger.Silent),
+			})
+		if err != nil {
+			return app, nil, err
+		}
+		app.PostgresConnection = db
+	}
 
 	return app, func() error {
-		return logger.Sync()
+		return app.Logger.Sync()
 	}, nil
 }
 
